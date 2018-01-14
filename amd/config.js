7237e7d07a461fc7d2de21fecad14bb6f9270307
EvgeniVasilev <evgeni.vasilev0912@gmail.com>
2018-01-14 19:57:21 +0200

    Initial commit.

--- amd/config.js
+++ amd/config.js
@@ -1,10 +1,10 @@
 requirejs.config({
-    baseUrl: "../build/js",
+    // baseUrl: "../build/js",
     paths: {
-        main: "main",
-        forms: "forms",
+        main: "../build/js/main",
+        forms: "../build/js/forms",
         jquery: ["https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min",
-            "libs/jquery-3.2.1.min"]
+            "../build/libs/jquery-3.2.1.min"]
 
     }
 });
