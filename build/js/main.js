43079c5181e7f351859544d74a8bfa8f8d283e74
EvgeniVasilev <evgeni.vasilev0912@gmail.com>
2018-01-15 12:55:01 +0200

    Remove adjust futer to bottom of the page functionality.

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-define(["jquery"],function(t){"use strict";var e=t("footer"),s=parseInt(e.css("height")),i=parseInt(t(window).height());e.css({position:"absolute",width:"100%",top:i-s+"px"})});
+define(["jquery"],function(e){"use strict";console.log("Works!")});
--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 define(["jquery"], function ($) {
   "use strict"
-  var footer = $("footer")
-  var footerHeight = parseInt(footer.css("height"))
-  var windowHeight = parseInt($(window).height())
-  
-  footer.css({
-    position: "absolute",
-    width: "100%",
-    top: (windowHeight - footerHeight) + "px"
-  })  
+  console.log("Works!");
 })
