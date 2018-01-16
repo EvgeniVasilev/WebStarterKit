739144a1d6aed94ab11424f7e6991e552748a8ac
EvgeniVasilev <evgeni.vasilev0912@gmail.com>
2018-01-16 14:49:08 +0200

    Add basik validation to register page.

--- js/forms.js
+++ js/forms.js
@@ -4,22 +4,37 @@ define(["jquery"], function ($) {
         $(this).find("input.form-input").each(function (i, elem) {
             if (elem.value === "") {
                 evt.preventDefault()
-                $(this).css({
-                    border: "2px solid red",
-                    background: "pink"
-                }).attr("placeholder", "Please fill in some value!")
+                $(this).addClass("dirty").attr("placeholder", "Please fill in some value!")
             } else {
-                $(this).css({
-                    border: "1px solid silver",
-                    background: "lightgreen"
-                })
+                $(this).addClass("valid")
             }
 
             $(this).focus(function () {
-                $(this).css({
-                    border: "1px solid silver",
-                    background: "white"
-                }).attr("placeholder", "")
+                $(this).addClass("input").attr("placeholder", "")
+            })
+
+            $(this).blur(function () {
+                if (elem.value !== "")
+                    $(this).css({
+                        border: "1px solid silver",
+                        background: "lightgreen"
+                    })
+            })
+        })
+    })
+
+
+    $(".register").submit(function (evt) {
+        $(this).find("input.form-input").each(function (i, elem) {
+            if (elem.value === "") {
+                evt.preventDefault()
+                $(this).addClass("dirty").attr("placeholder", "Please fill in some value!")
+            } else {
+                $(this).addClass("valid")
+            }
+
+            $(this).focus(function () {
+                $(this).addClass("input").attr("placeholder", "")
             })
 
             $(this).blur(function () {
