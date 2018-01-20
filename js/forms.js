a71f07854a73d4d7313e4fb1d912f56da0d55d22
EvgeniVasilev <evgeni.vasilev0912@gmail.com>
2018-01-20 22:44:01 +0200

    Forms validation improvements

--- js/forms.js
+++ js/forms.js
@@ -1,73 +1,123 @@
-define(["jquery"], function ($) {
-    "use strict"
+define(['jquery'], function ($) {
+  'use strict'
 
-    function emptyOrNotValidation(parent, child, evt) {
-        $(parent).find(child).each(function (i, elem) {
-            if ($(this).val() === "") {
-                evt.preventDefault()
-                $(this).addClass("dirty").attr("placeholder", "Please fill in some value!")
-            } else {
-                $(this).removeClass("input")
-                $(this).addClass("valid")
-            }
+  function emptyOrNotValidation (parent, child, evt) {
+    $(parent).find(child).each(function (i, elem) {
+      if ($(this).val() === '') {
+        evt.preventDefault()
+        $(this).addClass('dirty').attr('placeholder', 'Please fill in some value!')
+      } else {
+        $(this).removeClass('input')
+        $(this).addClass('valid')
+      }
 
-            $(this).focus(function () {
-                $(this).addClass("input").attr("placeholder", "")
-            })
+      $(this).focus(function () {
+        $(this).addClass('input').attr('placeholder', '')
+      })
 
-            $(this).blur(function () {
-                if ($(this).val() !== "") {
-                    $(this).removeClass("input")
-                    $(this).addClass("valid")
-                } else {
-                    $(this).removeClass("valid")
-                    $(this).removeClass("input")
-                    $(this).addClass("dirty").attr("placeholder", "Please fill in some value!")
-                }
-            })
-        })
-    }
-
-    function isValidEmail(evt) {
-        var e_mailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/,
-            email = $("#email")
-
-        
-        if (email.val() && !email.val().match(e_mailValid)) {
-            email.removeClass("dirty")
-            email.removeClass("input")
-            email.addClass("invalid_email")
-            alert("Please insert a valid email!");
-
-            email.focus(function () {
-                email.removeClass("valid")
-                email.addClass("input")
-            })
-            evt.preventDefault()    
+      $(this).blur(function () {
+        if ($(this).val() !== '') {
+          $(this).removeClass('input')
+          $(this).addClass('valid')
+        } else {
+          $(this).removeClass('valid')
+          $(this).removeClass('input')
+          $(this).addClass('dirty').attr('placeholder', 'Please fill in some value!')
         }
+      })
+    })
+  }
 
-        email.blur(function () {
-            if (email.val().match(e_mailValid)) {
-                email.removeClass("input")
-                email.removeClass("invalid_email")
-                email.addClass("valid")
-            }
-        })
+  function isValidEmail (evt) {
+    var emailValid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/
+    var email = $('#email')
 
+    email.removeClass('bad-value')
+
+    if (email.val() && !email.val().match(emailValid)) {
+      email.removeClass('dirty')
+      email.removeClass('input')
+      email.addClass('bad-value')
+      alert('Please insert a valid email!')
+
+      email.focus(function () {
+        email.removeClass('valid')
+        email.addClass('input')
+      })
+      evt.preventDefault()
     }
 
-    function validatePasswordRepeat() {
-        // TODO
+    email.keyup(function () {
+      if (email.val().match(emailValid)) {
+        email.removeClass('input')
+        email.removeClass('bad-value')
+        email.addClass('valid')
+      } else {
+        email.addClass('bad-value')
+      }
+    })
+  }
+
+  function validatePassword (evt) {
+    var password = $('#password')
+    var repeatePassword = $('#rep-password')
+    var isValidPassword = true
+
+    password.removeClass('bad-value')
+    repeatePassword.removeClass('bad-value')
+
+    if ((password.val() !== '') &&
+     (password.val().length < 8)) {
+      password.addClass('bad-value')
+      alert('Password must be et least 8 characters!')
+      evt.preventDefault()
+      isValidPassword = false
+    } else {
+      isValidPassword = true
     }
 
-
-    $(".login").submit(function (evt) {
-        emptyOrNotValidation(".login", "input.form-input", evt)
+    password.focus(function () {
+      password.removeClass('bad-value')
+      password.removeClass('valid')
+      password.removeClass('dirty')
+      password.addClass('input')
     })
 
-
-    $(".register").submit(function (evt) {
-        emptyOrNotValidation(".register", "input.form-input", evt)
-        isValidEmail(evt)
+    password.keyup(function () {
+      if ((password.val() !== '') &&
+       (password.val().length >= 8)) {
+        password.removeClass('bad-value')
+        password.addClass('valid')
+      } else if (password.val().length < 8) {
+        password.addClass('bad-value')
+      }
     })
+    if ((isValidPassword) && (password.val() !== repeatePassword.val())) {
+      repeatePassword.addClass('bad-value')
+      alert('Passwords does not match!')
+      evt.preventDefault()
+    }
+
+    repeatePassword.focus(function () {
+      repeatePassword.removeClass('valid')
+      repeatePassword.addClass('input')
+    })
+
+    repeatePassword.keyup(function () {
+      if ((isValidPassword) && (password.val() === repeatePassword.val())) {
+        repeatePassword.removeClass('bad-value')
+        repeatePassword.addClass('valid')
+      }
+    })
+  }
+
+  $('.login').submit(function (evt) {
+    emptyOrNotValidation('.login', 'input.form-input', evt)
+  })
+
+  $('.register').submit(function (evt) {
+    emptyOrNotValidation('.register', 'input.form-input', evt)
+    isValidEmail(evt)
+    validatePassword(evt)
+  })
 })
